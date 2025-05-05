b5121b668d5379178e1cf03f0d9e626985139d36
tripathidivyansh <tripatidivyansh144@gmail.com>
2025-05-06 03:13:52 +0530

    PHP data type

---
+++ data_type.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "Bro Code";
+    $food = "pizza";
+    // echo $name;
+    $email = "fake@gmail.com";
+
+    $age = 21;
+
+    echo "Hello {$name} \n";
+
+    echo "you like {$food} \n";
+    echo "your email is {$email} \n";
+    echo "your age {$age} year old";
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo "Hello, World! \n";
+echo "Hello, World!";
+// this is a comment
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order a pizza</button>
+</body>
+</html>
